f85d28c8746b89bdc1bd79072453f584788027a8
cj <thatcat13@gmail.com>
2018-07-31 15:31:40 -0700

    add testing suite with karma and jasmine; convert JS to ES6

---
+++ spec/spec.js
@@ -0,0 +1,15 @@
+//test ideas:
+//there are 52 'cards' in the deck
+//all cards have a value and a suit
+//sort function returns proper ascending numerical order
+//shuffle function returns randomized order
+
+const Deck = require('../index.js');
+const newDeck = new Deck();
+
+describe('Deck', function() {
+
+  it('should test whether an array representing deck of cards is sorted in ascending numerical order', function() {
+    //Test content will go here.
+  });
+});
